f22907c041ceaacbca43e7cb83dc8d9d8f745a69
mr sivasankar <sivasankar.k@creativebees.in>
2025-03-04 14:28:40 +0530

    added

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,11 @@ const {
     APP_API_URL : appUrl = 'https://hostel-production.up.railway.app'
 } = process.env
 
+// const {
+//     APP_API_URL : appUrl = 'http://localhost:1005'
+// } = process.env
+
+
 module.exports = {
     appUrl
 }
